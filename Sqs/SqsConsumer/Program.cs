781f5d5d09c2a365389d1cd31dfd5065b6d674bd
Anusha Ramadugu <ramaduguanusha123@gmail.com>
2025-04-12 08:46:10 +1000

    implemented SQS - Publisher consumer pattern

---
+++ Sqs/SqsConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+var sqsClient =  new AmazonSQSClient();
+
+var queueUrlResponse =await sqsClient.GetQueueUrlAsync("Customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string>{"All"},
+    MessageAttributeNames =  new List<string>{"All"},
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var res = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+    Console.WriteLine("Message received" + res.Messages?.Count);
+    if(res.Messages?.Count == 0 || res.Messages == null) break;
+    foreach (var message in res.Messages)
+    {
+        Console.WriteLine(message);
+        Console.WriteLine($"message id: {message.MessageId}");
+        Console.WriteLine($"message body: {message.Body}");
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle, cts.Token);
+    }
+
+    await Task.Delay(3000);
+    
+}
--- Sqs/SqsPublisher/Program.cs
+++ Sqs/SqsPublisher/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqs = new AmazonSQSClient();
+
+var message = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    FullName = "Anusha Ramadugu",
+    Email = "abc@gmail.com",
+    DateOfBirth = new DateOnly(1985, 10, 10),
+};
+var queueUrlResponse = await sqs.GetQueueUrlAsync("Customers");
+
+var messageRequest = new SendMessageRequest
+{
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue()
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    },
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(message),
+};
+
+var response = await sqs.SendMessageAsync(messageRequest);
+
+Console.WriteLine($"Sent message to {queueUrlResponse.QueueUrl}");
